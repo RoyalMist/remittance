6cd4ff237903ffaaf153a01901973019bf7df1ab
PrisM <thibault.fouache@gmail.com>
2019-05-07 22:18:16 +0200

    Try something else to follow Adel's questions :)

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -12,7 +12,7 @@ contract Remittance is Ownable {
     mapping(address => bytes32) private exchangePasswords;
 
     // Usage of the fiat user OTP as a key to retrieve the desired transaction.
-    mapping(byte32 => Transaction) private transactions;
+    mapping(bytes32 => Transaction) private transactions;
 
     event LogInitTransaction(address indexed initiator, address exchange, uint howMuch);
     event LogSetExchangePassword(address indexed initiator);
@@ -20,7 +20,7 @@ contract Remittance is Ownable {
 
     function initTransaction(address _exchange, bytes32 _fiatOTP) onlyOwner payable public {
         require(_exchange != address(0x0) && msg.value > 0, "Please provide a valid exchange address and a deposit greater than 0");
-        require(transactions[_fiatOTP].exchange == address(0x0), "This password is already used");
+        require(transactions[_fiatOTP].exchange != address(0x0), "This password is already used");
         emit LogInitTransaction(msg.sender, _exchange, msg.value);
         transactions[_fiatOTP] = Transaction({exchange : _exchange, amount : msg.value});
     }
@@ -30,11 +30,17 @@ contract Remittance is Ownable {
         exchangePasswords[msg.sender] = _exchangePassword;
     }
 
-    function withdraw(string memory _exchange_otp, string memory _debtor_otp) public {
-        //TODO
+    function withdraw(bytes32 _exchange_otp, bytes32 _debtor_otp) public {
+        require(hash(_debtor_otp) == exchangePasswords[msg.sender], "Wrong exchange password");
+        bytes32 hashed = hash(_exchange_otp);
+        Transaction memory t = transactions[hashed];
+        require(t.exchange != address(0x0), "Wrong fiat user password");
+        require(t.exchange == msg.sender, "You are not the exchange selected for this operation.");
+        address(msg.sender).transfer(t.amount);
+        delete (transactions[hashed]);
     }
 
-    function hash(string memory _password) view private returns (bytes32 hash) {
-        hash = keccak256(abi.encodePacked(address(this), exchange, _password));
+    function hash(bytes32 _password) view public returns (bytes32 hash) {
+        hash = keccak256(abi.encodePacked(address(this), _password));
     }
 }
