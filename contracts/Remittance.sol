94eee114f4af7c485af08d673058b9432935f4ed
PrisM <thibault.fouache@gmail.com>
2019-05-07 22:27:30 +0200

    Documentation and emit of event.

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -18,6 +18,7 @@ contract Remittance is Ownable {
     event LogSetExchangePassword(address indexed initiator);
     event LogWithdraw(address indexed initiator, uint howMuch);
 
+    // Instead of instantiating a contract per transaction, permits the creation of a infinite number of transactions.
     function initTransaction(address _exchange, bytes32 _fiatOTP) onlyOwner payable public {
         require(_exchange != address(0x0) && msg.value > 0, "Please provide a valid exchange address and a deposit greater than 0");
         require(transactions[_fiatOTP].exchange != address(0x0), "This password is already used");
@@ -25,21 +26,25 @@ contract Remittance is Ownable {
         transactions[_fiatOTP] = Transaction({exchange : _exchange, amount : msg.value});
     }
 
+    // Exchanges can change their password at anytime.
     function setExchangePassword(bytes32 _exchangePassword) public {
         emit LogSetExchangePassword(msg.sender);
         exchangePasswords[msg.sender] = _exchangePassword;
     }
 
+    // Use the fiat user otp to retriev transaction and check against exchange otp.
     function withdraw(bytes32 _exchange_otp, bytes32 _debtor_otp) public {
         require(hash(_debtor_otp) == exchangePasswords[msg.sender], "Wrong exchange password");
         bytes32 hashed = hash(_exchange_otp);
         Transaction memory t = transactions[hashed];
         require(t.exchange != address(0x0), "Wrong fiat user password");
         require(t.exchange == msg.sender, "You are not the exchange selected for this operation.");
-        address(msg.sender).transfer(t.amount);
+        emit LogWithdraw(msg.sender, t.amount);
         delete (transactions[hashed]);
+        address(msg.sender).transfer(t.amount);
     }
 
+    // Use contract address as a salt. May be not sufficient to protect from rainbow tables as Slat is known :(
     function hash(bytes32 _password) view public returns (bytes32 hash) {
         hash = keccak256(abi.encodePacked(address(this), _password));
     }
