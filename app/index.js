f621a0896ca25599af6d9c2601c5970d57fa3389
PrisM <thibault.fouache@gmail.com>
2019-05-15 22:36:31 +0200

    Start UI with Vue.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
 require('milligram');
+import Vue from 'vue'
+import Home from './vues/home'
+
+new Vue({
+    el: '#app',
+    render: h => h(Home)
+});
---
+++ app/vues/home.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Home</p>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+
+</style>
